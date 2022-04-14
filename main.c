c10ffeef11b6a2f466e0d003764ce79bd1aaa794
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2022-04-14 15:43:13 +0200

    setup corrected

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 19:22:04 by maabidal          #+#    #+#             */
-/*   Updated: 2022/04/13 21:41:36 by maabidal         ###   ########.fr       */
+/*   Updated: 2022/04/14 15:42:53 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,31 +52,36 @@ static t_general setup_general(int nb_philo, char **rest_av)
 	else
 		general.max_meals = ft_atoi(rest_av[3]);
 	general.sim_start = get_time();
-printf("time_to_die = %ld\ntime_to_eat = %ld\n time_to_sleep = %ld\n max_meals = %d\ngeneral.sim_start = %ld\n", general.time_to_die, general.time_to_eat, general.time_to_sleep, general.max_meals, general.sim_start);
+//printf("time_to_die = %ld\ntime_to_eat = %ld\n time_to_sleep = %ld\n max_meals = %d\ngeneral.sim_start = %ld\n", general.time_to_die, general.time_to_eat, general.time_to_sleep, general.max_meals, general.sim_start);
 	return (general);
 }
 
 static t_philo setup_philo(int index, int nb_philo, t_sa *forks, t_general *general)
 {
 	t_philo	philo;
+	int		limit;
 
 	philo.last_meal_time = general->sim_start;
-	if (index < nb_philo / 2)
+
+limit = nb_philo / 2 + nb_philo % 2;
+	if (index < limit)
 		philo.id = index * 2;
 	else
-		philo.id = index * 2 - nb_philo / 2;
+		philo.id = (index - limit) * 2 + 1;
 	philo.rf = forks + philo.id;
 	if (nb_philo <= 1)
 		philo.lf = NULL;
 	else
 		philo.lf = forks + ((philo.id + 1) % nb_philo);
 	philo.time_eaten = 0;
+/*
 printf("to philo %d\n", index);
-printf("last meal	= %ld\n", philo.last_meal_time);
-printf("id			= %d\n", philo.id);
-printf("rf			= %p\n", philo.rf);
-printf("lf			= %p\n", philo.lf);
-printf("time eaten	= %d\n", philo.time_eaten);
+printf("last meal   = %ld\n", philo.last_meal_time);
+printf("id = %d\n", philo.id);
+printf("rf          = %p\n", philo.rf);
+printf("lf          = %p\n", philo.lf);
+printf("time eaten  = %d\n\n", philo.time_eaten);
+*/
 	return (philo);
 }
 
@@ -96,7 +101,7 @@ static int launch_philo(t_ref ref, t_general general, t_sa *es, t_sa *forks)
 
 	if (ref.index >= ref.nb_philo)
 		return (0);
-printf("index = %d\n", ref.index);
+//printf("index = %d\n", ref.index);
 	philo = setup_philo(ref.index++, ref.nb_philo, forks, &general);
 	to_philo.philo = &philo;
 	to_philo.general = &general;
